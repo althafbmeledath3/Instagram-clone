ccb8fab25ff34247a56c1f6fe604f09671a1e091
kazuto0p <umamahesh9447230@gmail.com>
2025-04-28 03:28:09 +0530

    rwrw

--- backEnd/server.js
+++ backEnd/server.js
@@ -9,11 +9,18 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 4000;
 
+// Allow preflight requests
+app.options('*', cors());
+
+
+// Apply CORS middleware globally
 app.use(cors({
-  origin: ['http://localhost:4000', 'https://instagram-clone-1-frontend1.onrender.com/'], // Update with your actual frontend URL
-  methods: 'GET,POST,PUT,DELETE',
-  allowedHeaders: 'Content-Type,Authorization',
-}));
+    origin: ['http://localhost:4000', 'https://instagram-clone-1-frontend1.onrender.com'], // Frontend URLs
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true, // If you are using cookies or sessions
+  }));
+  
 
 app.use(express.json({ limit: '50mb' }));
 
