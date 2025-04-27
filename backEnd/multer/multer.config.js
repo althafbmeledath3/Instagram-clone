869c616da030cdb63432804495a2243ec4549f31
kazuto0p <umamahesh9447230@gmail.com>
2025-04-28 02:04:45 +0530

    fdsfs

--- backEnd/multer/multer.config.js
+++ backEnd/multer/multer.config.js
@@ -1,21 +1,38 @@
-import multer from "multer";
+// import multer from "multer";
 
-const storage = multer.diskStorage({
+// const storage = multer.diskStorage({
 
-    destination:(req,file,cb)=>cb(null,"images/"),
+//     destination:(req,file,cb)=>cb(null,"images/"),
 
-    filename:(req,file,cb)=>{
+//     filename:(req,file,cb)=>{
 
-        cb(null,Date.now()+ "-" +file.originalname)
+//         cb(null,Date.now()+ "-" +file.originalname)
+//     }
+// })
+
+
+// const upload = multer({storage})
+
+// export default upload
+
+
+
+import multer from 'multer';
+
+const storage = multer.memoryStorage(); // Use memory storage for Cloudinary
+const upload = multer({
+  storage,
+  limits: { fileSize: 50 * 1024 * 1024 }, // 50MB limit
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype.startsWith('image/')) {
+      cb(null, true);
+    } else {
+      cb(new Error('Only images are allowed'), false);
     }
-})
+  },
+});
 
-
-const upload = multer({storage})
-
-export default upload
-
-
+export default upload;
 
 
 
